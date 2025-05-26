86d5b4366fd474015ad1db400f6748528e9770b8
nichlazzz <koley-2015@mail.ru>
2025-05-26 19:47:48 +0300

    formatter

--- manage.py
+++ manage.py
@@ -2,8 +2,9 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings') # указывает настройки проекта
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')  # указывает настройки проекта
     try:
         from django.core.management import execute_from_command_line  # импортирует механизм команд
     except ImportError as exc:
@@ -11,6 +12,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable?"
         ) from exc
     execute_from_command_line(sys.argv)  # запускает команду из аргументов командной строки
+
 
 if __name__ == '__main__':
     main()
